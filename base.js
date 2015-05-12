110ac81f355140292041a572fca735cabe9a5e2e
ctstanley <ctstanley1@hotmail.com>
2015-05-11 21:56:28 -0700

    almost done

--- base.js
+++ base.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
-  // code in here!
+  // press enter to search
+  alert("Ready?");
+  $("#input").keypress(function(e){
+  	var keycode = (e.keyCode ? e.keyCode : e.which)
+  	if (keycode == '13'){
+  		var valu = $("#input").val();
+  		$(".container-fluid").empty();
+  		$.get("http://api.giphy.com/v1/gifs/search?q='" + valu + "'&api_key=dc6zaTOxFJmzC", render);
+  	}
+  });
+ $.get("http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC", render);
+   
+    // 
+
+function render(responseData) {
+    var data = responseData.data;
+    data.forEach(function(item, i) {
+        var thumb = item.images.fixed_height.url;
+        $(".container-fluid").append("<img src='" + thumb + "'>"); // mind the single vs. double quotes!
+    })
+}
   console.log("here's your data:", mockData)
 })
